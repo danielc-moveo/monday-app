0dcfb94e34289b7a0a2285a40b7c5d259e019c7d
danielc-moveo <danielc@moveohls.com>
2021-06-27 09:58:04 +0300

    cleanup

--- src/api/monday/index.js
+++ src/api/monday/index.js
@@ -1,8 +1,3 @@
-import mondaySdk from "monday-sdk-js";
+import mondaySdk from 'monday-sdk-js';
 
-// export const mondayUserInstance = mondaySdk({});
-
-export const mondayUserInstance = mondaySdk({
-  // clientId: process.env.REACT_APP_CLIENT_ID,
-  // apiToken: process.env.REACT_APP_API_TOKEN,
-});
+export const mondayUserInstance = mondaySdk({});
